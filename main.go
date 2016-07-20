7340f2481a5364d8596fbcedc5b6857314843a02
Maude Lemaire <maude.lemaire@mail.mcgill.ca>
2016-07-20 00:24:08 -0400

    Create generates a .cap directory, a refs directory within the .cap directory, and a main file that contains the hash of the current commit. Commit creates a blob from file.txt's hashed contents; it also creats a JSON file specifying the blob associated with it.

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
-import "os"
-import "fmt"
-import "log"
-import "io/ioutil"
-import "github.com/codahale/blake2"
+import (
+	"os"
+	// "fmt"
+	"log"
+	"encoding/hex"
+	"io/ioutil"
+	"github.com/codahale/blake2"
+	"encoding/json"
+)
 
 const filename = "file.txt"
+const directory = ".cap"
+const refs = "refs"
+
+//Create a "directory" string and a "file" string
+//in order to create easy helper functions to string 
+//concat everything when generating new files
 
 var commands = map[string]func(){
 	"commit": commit,
@@ -16,14 +26,24 @@ var commands = map[string]func(){
 }
 
 func main() {
-	commands[os.Args[1]]()
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a valid cap command.")
+	} else {
+		commands[os.Args[1]]()
+	}
 }
 
 func create() {
-	err := os.Mkdir(".cap", 0777)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//Create the .cap directory.
+	err := os.Mkdir(directory, 0777)
+	checkError(err)
+	//Create the .refs directory.
+	err = os.Mkdir(relativePath(refs), 0777)
+	checkError(err)
+	//Create an empty "main" branch ref.
+	refFile, err := os.Create(".cap/refs/main")
+	checkError(err)
+	refFile.Close()
 }
 
 //Record the blob
@@ -32,12 +52,26 @@ func create() {
 //Update the ref (hash of commit)
 func commit() {
 	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	hash = blake2.NewBlake2B()
+	checkError(err)
+	hash := blake2.NewBlake2B()
 	hash.Write(bytes)
-	sum := hash.Sum(nilf)
+	sum := hash.Sum(nil)
+	hexValue := hex.EncodeToString(sum)
+	//Determine when to use the hex value and when to use the actual hash encoding.
+	blobErr := ioutil.WriteFile(relativePath("blob"), []byte(hexValue), 0777)
+	checkError(blobErr)
+	commitErr := ioutil.WriteFile(relativePath("commit.json"), generateCommit(hexValue), 0777)
+	checkError(commitErr)
+	refFileErr := ioutil.WriteFile(".cap/refs/main", []byte(hexValue), 0777)
+	checkError(refFileErr)
+}
+
+//Get current commit hash in ref
+//Set new commit to point to commit hash in ref
+func generateCommit(hexValue string) []byte {
+	attributes := map[string]string{"blob": string(hexValue), "previousCommit": "none"}
+	commitContent, _ := json.Marshal(attributes)
+	return commitContent
 }
 
 //Looking at the other ("remote") copy of the repo
@@ -55,4 +89,14 @@ func pull() {
 
 func push() {
 
+}
+
+func checkError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func relativePath(file string) (filename string) {
+	return directory + "/" + file
 }
